ebb1afbfaabf0c1d49953b543d9e88ecc1835146
Andrew Brandt <abrandt@makeschool.com>
2015-07-11 23:09:00 -0700

    Add graceNote framework

---
+++ OutsideLandsScavenger/GNHelper.swift
@@ -0,0 +1,19 @@
+//
+//  GNHelper.swift
+//  OutsideLandsScavenger
+//
+//  Created by Andrew Brandt on 7/11/15.
+//  Copyright (c) 2015 Lynne Okada. All rights reserved.
+//
+
+import UIKit
+
+class GNHelper: NSObject {
+    
+    
+    
+    override init() {
+    
+    }
+   
+}
